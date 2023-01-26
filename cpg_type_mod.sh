8971150313b537aad13bd52b6c1bd48dcf4057ec
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2023-01-26 14:19:57 +0000

    Generate type info

---
+++ cpg_type_mod.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+/home/gmap/mrpm/marguina/fxtran-acdc/bin/generateStructureMethods.pl \
+ --field-api --field-api-class info_var src/local/.fypp/arpifs/module/variable_module.F90
+
+/home/gmap/mrpm/marguina/fxtran-acdc/bin/generateStructureMethods.pl \
+ --field-api --field-api-class info_var src/local/.fypp/arpifs/module/field_variables_mod.F90
+
+/home/gmap/mrpm/marguina/fxtran-acdc/bin/generateStructureMethods.pl \
+ --field-api --field-api-class info_cpg src/local/.fypp/arpifs/module/mf_phys_type_mod.F90
+
+/home/gmap/mrpm/marguina/fxtran-acdc/bin/generateStructureMethods.pl \
+ --field-api --field-api-class info_cpg src/local/.fypp/arpifs/module/mf_phys_base_state_type_mod.F90
+
+/home/gmap/mrpm/marguina/fxtran-acdc/bin/generateStructureMethods.pl \
+ --field-api --field-api-class info_cpg src/local/.fypp/arpifs/module/mf_phys_next_state_type_mod.F90
+
+/home/gmap/mrpm/marguina/fxtran-acdc/bin/generateStructureMethods.pl \
+ --field-api --field-api-class info_cpg src/local/.fypp/arpifs/module/cpg_type_mod.F90
+
+/home/gmap/mrpm/marguina/fxtran-acdc/bin/generateStructureMethods.pl \
+ --field-api --field-api-class info_sfv src/local/.fypp/arpifs/module/surface_views_diagnostic_module.F90
+
+/home/gmap/mrpm/marguina/fxtran-acdc/bin/generateStructureMethods.pl \
+ --field-api --field-api-class info_sfv src/local/.fypp/arpifs/module/surface_views_prognostic_module.F90
+
+/home/gmap/mrpm/marguina/fxtran-acdc/bin/generateStructureMethods.pl \
+ --field-api --field-api-class info_sfc src/local/.fypp/arpifs/module/surface_variables_mod.F90
+
+/home/gmap/mrpm/marguina/fxtran-acdc/bin/generateStructureMethods.pl \
+ --field-api --field-api-class info_sfc src/local/.fypp/arpifs/module/mf_phys_surface_type_mod.F90
+
+./scripts/linkTypes.pl
+
+
